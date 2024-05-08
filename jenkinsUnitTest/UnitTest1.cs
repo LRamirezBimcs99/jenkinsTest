9132b0d4c1d1c155d035e024032dbf5f115d3647
Leon Ramirez <apoca1999@gmail.com>
2024-05-07 23:19:00 -0500

    test added

--- jenkinsUnitTest/UnitTest1.cs
+++ jenkinsUnitTest/UnitTest1.cs
@@ -1,16 +1,31 @@
+using jenkinsTest;
+using jenkinsTest.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+
 namespace jenkinsUnitTest
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
+        private WeatherForecastController _weatherForecastController;
+        private Mock<ILogger<WeatherForecastController>> _logger;
+        private const string Expected = "Hello World! from Thetips4you";
+
         public void Setup()
         {
+            _logger = new Mock<ILogger<WeatherForecastController>>();
+            _weatherForecastController = new WeatherForecastController(_logger.Object);
         }
 
         [Test]
-        public void Test1()
+        public void TestMethod1()
         {
-            Assert.Pass();
+            Setup();
+            var response = _weatherForecastController.Get();
+
+            Assert.IsInstanceOf(typeof(WeatherForecast[]), response);
+
         }
     }
 }
